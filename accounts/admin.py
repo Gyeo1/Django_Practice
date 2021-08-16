c6b3f56f3a7788cb3b4916fb9b144f5a6e1fc53f
Gyeo1 <rlatjdruf99@naver.com>
2021-08-16 16:38:29 +0900

    O2O 필드 실습

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
+
+@admin.register(Profile)#여기 부분이 실제 웹 페이지에서 등록하는 부분이다.
+class ProfileAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
