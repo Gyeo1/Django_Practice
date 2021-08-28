56abc837934bd33c70a735f1dd569f6a8f6f68bc
Gyeo1 <rlatjdruf99@naver.com>
2021-08-28 12:45:30 +0900

    messages 등록/소비

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.decorators import method_decorator
@@ -15,6 +16,8 @@ def post_new(request):
             post = form.save(commit=False)  # 필요에 따라 DB에 저장하기 #commit을 False로 두면 post.save가 아직 안옴
             post.author = request.user  # 현재 로그인 유저 인스턴스를 받아옴
             post.save()  # 받고 나서 post.save 실행 // 주의사항! 로그인을 보장 받아야 된다. 따라서 @ 위에다 붙이기
+
+            messages.success(request, '포스팅을 저장했습니다.')  # messages 등록!
             return redirect(post)
         else:
             form.errors #검증실패시 오류정보 저장.
@@ -37,6 +40,8 @@ def post_edit(request,pk):
         form=PostForm(request.POST, request.FILES, instance=post) #POST와 파일을 인자값으로
         if form.is_valid(): # 인자로 받은 값에 대해서 유효성을 검증 , 검증 성공한 값을 사전형으로 제공받음 제대로 제공 받으면 FORM의 역할끝!
             post=form.save()    #필요에 따라 DB에 저장하기 #commit을 False로 두면 post.save가 아직 안옴
+
+            messages.success(request, '포스팅을 수정했습니다.')  # messages 등록!
             return redirect(post)
         else:
             form.errors #검증실패시 오류정보 저장.
@@ -53,17 +58,21 @@ def post_edit(request,pk):
 #     q=request.GET.get('q','') #''은  키가 없을 때 빈칸을 반환 한다는 의미이다.
 #     if q: #q라는 검색어가 있다면 여서긴 빈칸도 가능하기 때문에 모든 내용을 가져옴
 #         qs=qs.filter(message__icontains=q) #qs즉 모든 객체에서 q라는 내용을 필터링해서 다시 넣는다.
+#
+#     messages.info(request,'messages Test입니다') #messages 등록!
+#
 #     return render(request,'instagram/post_list.html',{
 #         'post_list':qs,
 #         'q':q,
 #     })
-post_list=ListView.as_view(model=Post,paginate_by=10) #이 한줄이 위의 내용을 포함한다. 근데 응용이 어렵다!
+# post_list=ListView.as_view(model=Post,paginate_by=10) #이 한줄이 위의 내용을 포함한다. 근데 응용이 어렵다!
 
 # @method_decorator(login_required,name='dispatch')
-# class PostList(ListView):
-#     model = Post
-#     paginate_by = 10
-# post_list=PostList.as_view()
+class PostList(LoginRequiredMixin,ListView):
+    model = Post
+    paginate_by = 10
+
+post_list=PostList.as_view()
 
 #render로 html응답을 받아온다. 장고의 template 시스템을 활용하기 위한 함수.
 #render함수의 가운데 경로는 실제 instagram 내부의 경로로 실제 경로를 만들어 줘야 된다.
