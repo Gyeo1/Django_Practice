85e2f94ea5480be4d1cb7662839cdc8e7ff03700
Gyeo1 <rlatjdruf99@naver.com>
2021-08-29 17:21:36 +0900

    edit 추가중

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
+from . import views
 urlpatterns=[
     path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('profile/',views.profile, name='profile'), #이렇게 하면 profile url로 이동시 views의 profile로 가 랜더링 된다.
+    path('profile/edit',views.profile_edit, name='profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+@login_required
+def profile(request):
+    return render(request,'accounts/profile.html')
+
+#아래는 위의 내용을 CBV로 만들 경우!
+# class ProfileView(LoginRequiredMixin,TemplateView):
+#     template_view='accounts/profile.html'
+# profile=ProfileView.as_view()
+
+def profile_edit(request):
+    render(request,'accounts/profile_edit.html')
