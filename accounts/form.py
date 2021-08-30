2e239f8dd7e79572b2bda81860af2360a88dc793
Gyeo1 <rlatjdruf99@naver.com>
2021-08-30 13:24:54 +0900

    로그인 커스텀 해보기(Form의 활용!)

--- accounts/form.py
+++ accounts/form.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Profile
-
+from django.contrib.auth.forms import AuthenticationForm
 class ProfileForm(forms.ModelForm):
     class Meta:
         model= Profile
         fields=['address' , 'zipcode'] #입력 받을 값!
+
+class LoginForm(AuthenticationForm):
+    answer=forms.IntegerField(help_text='3+3=?')
+    def clean_answer(self):
+        answer=self.cleaned_data.get('answer')
+        if answer !=6:
+            raise forms.ValidationError("틀렸습니다!")
+        return answer
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from . import form
 urlpatterns=[
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', LoginView.as_view(
+        form_class=form.LoginForm,
+        template_name='accounts/login_form.html'), name='login'),
     path('profile/',views.profile, name='profile'), #이렇게 하면 profile url로 이동시 views의 profile로 가 랜더링 된다.
     path('profile/edit',views.profile_edit, name='profile_edit'),
     path('signup/',views.signup, name='signup'),
