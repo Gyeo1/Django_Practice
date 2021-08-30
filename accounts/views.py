5985af641bfc9c0ab989ba66c226234cc46aa8ae
Gyeo1 <rlatjdruf99@naver.com>
2021-08-30 13:57:03 +0900

    회원가입 하자마자 자동 로그인 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model , login as auth_login
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -42,14 +42,19 @@ def profile_edit(request):
 #     model = Profile
 #     form_class = ProfileForm
 # profile_edit = ProfileUpdateView.as_view()
-
 User=get_user_model() #절대models에서 받아오지 마라!
-signup=CreateView.as_view(
-    model=User,
-    form_class=UserCreationForm,
-    success_url=settings.LOGIN_URL,
-    template_name='accounts/signup_form.html'
-)
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = 'accounts/signup_form.html'
+    def form_valid(self, form):
+        response=super().form_valid(form) #부모 호출해서 응답을 받고
+        user=self.object #self.object를 그래도 참조
+        auth_login(self.request,user) #로그인 기능 수행
+        return response #CreateView의 form_valid의 방식에 따라 구현
+
+signup=SignUpView.as_view()
 
 def logout(request):
     pass
